973c49541dcb988778cf24ebbc4614f85d6bfaca
Jeremy <esudious@gmail.com>
2017-04-23 23:30:50 -0600

    .

--- TableTrafficLight.c
+++ TableTrafficLight.c
@@ -2,28 +2,7 @@
 // Runs on LM4F120 or TM4C123
 // Index implementation of a Moore finite state machine to operate
 // a traffic light.
-// Daniel Valvano, Jonathan Valvano
-// July 20, 2013
 
-/* This example accompanies the book
-   "Embedded Systems: Introduction to ARM Cortex M Microcontrollers",
-   ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2013
-   Volume 1 Program 6.8, Example 6.4
-   "Embedded Systems: Real Time Interfacing to ARM Cortex M Microcontrollers",
-   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2013
-   Volume 2 Program 3.1, Example 3.1
-
- Copyright 2013 by Jonathan W. Valvano, valvano@mail.utexas.edu
-    You may use, edit, run or distribute this file
-    as long as the above copyright notice remains
- THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
- OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
- */
 
 // east facing red light connected to PB5
 // east facing yellow light connected to PB4
@@ -34,10 +13,12 @@
 // north facing car detector connected to PE1 (1=car present)
 // east facing car detector connected to PE0 (1=car present)
 
+#include "TExaS.h"
+#include "tm4c123gh6pm.h"
 #include "PLL.h"
 #include "SysTick.h"
 
-#define LIGHT                   (*((volatile unsigned long *)0x400050FC))
+#define STREET_LIGHT            (*((volatile unsigned long *)0x400050FC))
 #define GPIO_PORTB_OUT          (*((volatile unsigned long *)0x400050FC)) // bits 5-0
 #define GPIO_PORTB_DIR_R        (*((volatile unsigned long *)0x40005400))
 #define GPIO_PORTB_AFSEL_R      (*((volatile unsigned long *)0x40005420))
@@ -66,17 +47,19 @@
 #define GPIO_PORTF_CR_R 		(*((volatile unsigned long *)0x40025524))
 #define SYSCTL_RCGCGPIO_R   (*((volatile unsigned long *)0x400FE608))
 #define GPIO_PORTF_LOCK_R   (*((volatile unsigned long *)0x40025520))
-#define GPIO_LOCK_KEY  			(*((volatile unsigned long *)0x4C4F434B))
 
 #define lng_wt 3000
 #define sht_wt 500
 
+void DisableInterrupts(void);
+void EnableInterrupts(void);
+void ports_init(void);
+
 // Linked data structure
 struct State {
   unsigned long Out; //LEDS
-  unsigned long Time;
-	unsigned long PF1;
-	unsigned long PF3;  
+	unsigned long Time;
+	unsigned long PF;
   unsigned long Next[8];
 }; 
 typedef const struct State STyp;
@@ -100,28 +83,69 @@ typedef const struct State STyp;
 #define walkBlink6 16
 #define walkBlink7 17
 
+#define northGreenLight  0x21 //0x01 , northRedLight 0x04
+#define northYellowLight 0x22 //0x02
+#define eastGreenLight   0x0C //0x08   eastRedLight 0x20
+#define eastYellowLight  0x14 //0x10
+#define allRed   				 0x24 //
+#define greenWalk				 0x02
+#define redWalk 				 0x08
 
-
-//OUT, TIME, NEXT STATE, PF3 (walk green), PF1 (walk red)
+//OUT, TIME, PF3 (walk green light)  N, E, N+E, P, E+P, N+P, All 
 STyp FSM[11]={
- {0x21,lng_wt,{goN,waitN,goN,waitN}}, //Go North (Green) 
- {0x22,sht_wt,{goE,goE,goE,goE}},  //Wait North (Yellow) next is East
- {0x0C,lng_wt,{goE,goE,waitE,waitE}}, //Go East (Green)
- {0x14,sht_wt,{goN,goN,goN,goN}},  //Wait East (Yellow) next is North
- {0x11,sht_wt,{walk,walk,walk,walk}}, //Walk
- {0x21,lng_wt,{goN,waitN,goN,waitN}}, 			//Go North Next is Wait to walk
- {0x22,sht_wt,{walk,walk,walk,walk}},  //Wait North (Yellow) next is Walk
- {0x0c,lng_wt,{}},			//Go East Next is Wait East to walk
-				//Wait East (Yellow) next is Walk 
+ {northGreenLight	,lng_wt,redWalk,	{goN,waitN,goN,waitN}}, //Go North (Green) 
+ {northYellowLight,sht_wt,redWalk,	{waitN,goE,goE,goE}},  //Wait North (Yellow) next is East
+ {eastGreenLight	,lng_wt,redWalk,	{goE,goE,waitE,waitE}}, //Go East (Green)
+ {eastYellowLight	,sht_wt,redWalk,	{waitE,goN,goN,goN}},  //Wait East (Yellow) next is North
+ {allRed					,sht_wt,greenWalk,{walk,walk,walk,walk}}, //Walk
+ {northGreenLight	,lng_wt,redWalk,	{goN,waitN,goN,waitN}}, 			//Go North Next is Wait to walk
+ {northYellowLight,sht_wt,redWalk,	{walk,walk,walk,walk}},  //Wait North (Yellow) next is Walk
+ {eastGreenLight	,lng_wt,redWalk,	{walk,walk,walk,walk}},			//Go East Next is Wait East to walk
+ {eastYellowLight	,sht_wt,redWalk,	{walk,walk,walk,walk}}, 																//Wait East (Yellow) next is Walk 
+ {allRed					,sht_wt,greenWalk,{walk,walk,walk,walk}},																		
+ {allRed					,sht_wt,redWalk ,	{walk,walk,walk,walk}}
 };  
 	
 unsigned long S;  // index to the current state 
 unsigned long Input; 
- 
-int main(void){ volatile unsigned long delay;
-  PLL_Init();       // 80 MHz, Program 10.1
+volatile unsigned long delay;
+
+int main(void){ 
+  PLL_Initx();       // 80 MHz, Program 10.1
   SysTick_Init();   // Program 10.2
-  SYSCTL_RCGC2_R |= 0x12;      // 1) B E
+	EnableInterrupts();
+  
+  S = goN;  
+	
+  while(1){
+    STREET_LIGHT = FSM[S].Out;  // set lights
+    SysTick_Wait10ms(FSM[S].Time);
+    Input = SENSOR;     // read sensors
+    S = FSM[S].Next[Input];  
+  }
+}
+
+void ports_init(){
+	// Port B
+  GPIO_PORTB_LOCK_R = 0x4C4F434B;   // unlock port
+  GPIO_PORTB_CR_R = 0x3F;           // allow changes to PB5-0
+	GPIO_PORTB_PCTL_R = 0x00000000;   // clear PCTL
+  GPIO_PORTB_AMSEL_R &= ~0x3F;      // disable analog on PB5-0
+  GPIO_PORTB_AFSEL_R &= ~0x3F;      // disable alt funct on PB5-0
+  GPIO_PORTB_DEN_R |= 0x3F;         // enable digital I/O on PB5-0
+	GPIO_PORTB_DIR_R |= 0x3F;         // PB5-0 outputs
+	
+	// Port E
+  GPIO_PORTE_LOCK_R = 0x4C4F434B;   // unlock port
+  GPIO_PORTE_CR_R = 0x07;           // allow changes to PE2-0
+	GPIO_PORTE_PCTL_R = 0x00000000;   // clear PCTL
+  GPIO_PORTE_AMSEL_R &= ~0x07;      // disable analog on PE2-0
+  GPIO_PORTE_AFSEL_R &= ~0x07;      // disable alt funct on PE2-0
+  GPIO_PORTE_PUR_R &= ~0x07;        // disableb pull-up on PE2-0
+  GPIO_PORTE_DEN_R |= 0x07;         // enable digital I/O on PE2-0
+	GPIO_PORTE_DIR_R &= ~0x07;        // PE2-0 inputs
+	
+	SYSCTL_RCGC2_R |= 0x12;      // 1) B E
   delay = SYSCTL_RCGC2_R;      // 2) no need to unlock
   GPIO_PORTE_AMSEL_R &= ~0x03; // 3) disable analog function on PE1-0
   GPIO_PORTE_PCTL_R &= ~0x000000FF; // 4) enable regular GPIO
@@ -140,13 +164,8 @@ int main(void){ volatile unsigned long delay;
   GPIO_PORTF_DIR_R |= 0x0A;          // 11) PF3 and PF1 outputs
   GPIO_PORTF_AFSEL_R &= ~0x0A; 			// 12) regular function on PF3 and PF1
   GPIO_PORTF_DEN_R |= 0x0A;    			// 13) enable digital on PF3 and PF1
-  S = goN;  
-	
-  while(1){
-    LIGHT = FSM[S].Out;  // set lights
-    SysTick_Wait10ms(FSM[S].Time);
-    Input = SENSOR;     // read sensors
-    S = FSM[S].Next[Input];  
-  }
 }
+
+
+
 
